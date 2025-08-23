078c42f220f690af391f6aaa5b92b9c799acb097
Jason Yang <Jasonyang@yangyaneMacBook.lan>
2025-08-23 20:39:08 +1000

    chore: test PR for assignment demonstration

---
+++ test-pr.js
@@ -0,0 +1,9 @@
+// Test file for demonstrating Pull Request process
+// This file is created to show the complete PR workflow
+// Student: Yanlongyang
+// Student Number: 22519263
+// Course: CSE3CWA / CSE5006
+
+console.log("Test PR demonstration");
+console.log("This file will be used to create a Pull Request");
+console.log("Then merged to show the purple 'Merged' status");
